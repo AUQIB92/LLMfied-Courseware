8537bf7a9544461501538330ef0f92b69bb49c7f
auqib92 <auqib92@gmail.com>
2025-07-03 07:03:56 +0530

    Improved Readability fix

--- app/profile/layout.js
+++
@@ -1,9 +0,0 @@
-import ProfileShell from "@/components/profile/ProfileShell"
-
-export default function ProfileLayout({ children }) {
-  return (
-    <ProfileShell>
-      {children}
-    </ProfileShell>
-  )
-}
